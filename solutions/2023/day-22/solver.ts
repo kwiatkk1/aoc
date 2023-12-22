c1a02ce7aba915747aa581424f899102771ae857
Krzysztof Kwiatkowski <kwiatkk1@gmail.com>
2023-12-22 18:29:22 +0100

    solution: day 22 (not so bruteforce)

--- solutions/2023/day-22/solver.ts
+++ solutions/2023/day-22/solver.ts
@@ -1,144 +1,97 @@
 type Brick = {
-  name: string;
-  from: Point;
-  to: Point;
-  cubes: Cube[];
-  supports?: Set<Brick>;
-  supportedBy?: Set<Brick>;
-};
-
-type Point = {
-  x: number;
-  y: number;
-  z: number;
+  min: Cube;
+  max: Cube;
+  supportsFor: Brick[];
+  supportedBy: Brick[];
 };
 
 type Cube = {
   x: number;
   y: number;
   z: number;
-  brick: Brick;
 };
 
 function parse(input: string): Brick[] {
-  const bricks = input.split("\n").map<Brick>((line, i) => {
-    const [from, to] = line.split("~");
-    const [x, y, z] = from.split(",").map(Number);
-    const [x2, y2, z2] = to.split(",").map(Number);
-    return {
-      name: `brick-${String.fromCharCode(i + 65)}`,
-      from: { x, y, z },
-      to: { x: x2, y: y2, z: z2 },
-      cubes: [],
-      supports: new Set(),
-      supportedBy: new Set(),
-    };
-  });
+  const bricks: Brick[] = input
+    .split("\n")
+    .map((line) => line.split(/[,~]/).map(Number))
+    .map(([x1, y1, z1, x2, y2, z2]) => ({
+      min: { x: Math.min(x1, x2), y: Math.min(y1, y2), z: Math.min(z1, z2) },
+      max: { x: Math.max(x1, x2), y: Math.max(y1, y2), z: Math.max(z1, z2) },
+      supportsFor: [],
+      supportedBy: [],
+    }))
+    .sort((a, b) => a.min.z - b.min.z);
 
+  // let all bricks to settle down
+  bricks.forEach((brick) => freeFall(brick, bricks));
+
+  // build a graph of bricks
   bricks.forEach((brick) => {
-    const cubes: Cube[] = [];
-    for (let x = brick.from.x; x <= brick.to.x; x++) {
-      for (let y = brick.from.y; y <= brick.to.y; y++) {
-        for (let z = brick.from.z; z <= brick.to.z; z++) {
-          cubes.push({ x, y, z, brick });
-        }
-      }
-    }
-
-    brick.cubes = cubes;
-  });
-
-  bricks.sort((a, b) => {
-    const minZa = Math.min(a.from.z, a.to.z);
-    const minZb = Math.min(b.from.z, b.to.z);
-    return minZa - minZb;
-  });
-
-  bricks.forEach((brick) => {
-    while (canMoveDown(brick, bricks)) moveDown(brick);
-  });
-
-  bricks.map((brick) => {
-    const supportCubes = findSupportCubes(brick, bricks);
-    const supportBricks = [
-      ...supportCubes.reduce(
-        (supportBricks, cube) => supportBricks.add(cube.brick),
-        new Set<Brick>()
-      ),
-    ];
-
-    brick.supportedBy = new Set(supportBricks);
-
-    supportBricks.forEach((supportBrick) => {
-      supportBrick.supports = supportBrick.supports || new Set();
-      supportBrick.supports.add(brick);
-    });
+    const supportedBy = findSupportBricks(brick, bricks);
+    brick.supportedBy = supportedBy;
+    supportedBy.forEach((support) => support.supportsFor.push(brick));
   });
 
   return bricks;
 }
 
-function canMoveDown(brick: Brick, stack: Brick[]): boolean {
-  const rest = stack.filter((b) => b !== brick);
-  const restCubes = rest.flatMap((b) => b.cubes);
+function getBricksUnder(brick: Brick, allBricks: Brick[]) {
+  return (
+    allBricks
+      // find all bricks that are beneath the brick
+      .filter((it) => it.max.z < brick.min.z)
 
-  return brick.cubes.every((c) => {
-    const inSameColumnBelow = restCubes.find(
-      (rc) => rc.x === c.x && rc.y === c.y && rc.z == c.z - 1
-    );
-    const aboveGround = c.z > 1;
-    return aboveGround && !inSameColumnBelow;
-  });
+      // leave only bricks that are intersecting on x
+      .filter((it) => it.min.x <= brick.max.x && it.max.x >= brick.min.x)
+
+      // leave only bricks that are intersecting on y
+      .filter((it) => it.min.y <= brick.max.y && it.max.y >= brick.min.y)
+
+      // sort to find the highest laying brick
+      .sort((a, b) => b.max.z - a.max.z)
+  );
 }
 
-function moveDown(brick: Brick) {
-  brick.cubes.forEach((c) => (c.z -= 1));
+function freeFall(brick: Brick, bricks: Brick[]) {
+  const [firstBrickUnder] = getBricksUnder(brick, bricks);
+  const brickHeight = brick.max.z - brick.min.z;
+  const dropPoint = firstBrickUnder ? firstBrickUnder.max.z + 1 : 0;
+
+  brick.min.z = dropPoint;
+  brick.max.z = dropPoint + brickHeight;
 }
 
-function findSupportCubes(brick: Brick, stack: Brick[]) {
-  const rest = stack.filter((b) => b !== brick);
-  const restCubes = rest.flatMap((b) => b.cubes);
-
-  return brick.cubes.flatMap((c) => {
-    const inSameColumnBelow = restCubes.find(
-      (rc) => rc.x === c.x && rc.y === c.y && rc.z == c.z - 1
-    );
-    return inSameColumnBelow ? [inSameColumnBelow] : [];
-  });
+function findSupportBricks(brick: Brick, stack: Brick[]) {
+  const levelBelow = brick.min.z - 1;
+  return getBricksUnder(brick, stack).filter((b) => b.max.z === levelBelow);
 }
 
 function isRequired(brick: Brick) {
-  const { supports = new Set() } = brick;
-
-  for (let supported of supports) {
-    const { supportedBy = new Set() } = supported;
-    if (supportedBy.size === 1) {
-      return true;
-    }
-  }
-
-  return false;
+  return brick.supportsFor.some((it) => it.supportedBy.length === 1);
 }
 
-function getFallCountIfRemoved(brick: Brick) {
-  const fallen = new Set<Brick>();
-  const toFall = [brick];
+function getImpactedCount(startBrick: Brick) {
+  const impactedBricks = new Set<Brick>();
+  const toFall = [startBrick];
 
   while (toFall.length > 0) {
-    const brick = toFall.shift()!;
-    fallen.add(brick);
+    const fallingBrick = toFall.shift()!;
+    impactedBricks.add(fallingBrick);
 
-    // fall all bricks that were supported by this brick
-    // and are not supported by any other not fallen brick
-    const newToFall = [...brick.supports!].filter((b) => {
-      const restSupport = [...b.supportedBy!].filter((sb) => !fallen.has(sb));
-      return !restSupport.length;
+    fallingBrick.supportsFor.forEach((brick) => {
+      const isImpacted = brick.supportedBy.every((support) =>
+        impactedBricks.has(support)
+      );
+      if (isImpacted) toFall.push(brick);
     });
-
-    toFall.push(...newToFall);
   }
 
-  return fallen.size - 1;
+  return impactedBricks.size - 1;
+}
+
+function sum(a: number, b: number) {
+  return a + b;
 }
 
 export function solvePart1(input: string): number {
@@ -146,8 +99,5 @@ export function solvePart1(input: string): number {
 }
 
 export function solvePart2(input: string): number {
-  return parse(input)
-    .filter((brick) => isRequired(brick))
-    .map((brick) => getFallCountIfRemoved(brick))
-    .reduce((sum, n) => sum + n, 0);
+  return parse(input).filter(isRequired).map(getImpactedCount).reduce(sum);
 }
