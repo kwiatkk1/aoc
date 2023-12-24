6ca46384db641f1dd733d31ae8941c4e243c8029
Krzysztof Kwiatkowski <kwiatkk1@gmail.com>
2023-12-24 17:31:45 +0100

    solution: day 24 (part 2: sad and not working bruteforce)

--- solutions/2023/day-24/solver.ts
+++ solutions/2023/day-24/solver.ts
@@ -10,6 +10,14 @@ type Point = {
   z: number;
 };
 
+function positionAt(hail: Hail, t: number): Point {
+  return {
+    x: hail.position.x + t * hail.velocity.x,
+    y: hail.position.y + t * hail.velocity.y,
+    z: hail.position.z + t * hail.velocity.z,
+  };
+}
+
 function getCollidingWithinArea(
   hails: Hail[],
   area: { min: number; max: number }
@@ -75,6 +83,25 @@ function parse(input: string) {
   });
 }
 
+function getLineFormula(p1: Point, p2: Point, t1: number, t2: number) {
+  const dt = t2 - t1;
+  const l = (p2.x - p1.x) / dt;
+  const m = (p2.y - p1.y) / dt;
+  const n = (p2.z - p1.z) / dt;
+
+  return (t: number): Point => {
+    return {
+      x: p1.x + (t - t1) * l,
+      y: p1.y + (t - t1) * m,
+      z: p1.z + (t - t1) * n,
+    };
+  };
+}
+
+function equals(p1: Point, p2: Point) {
+  return p1.x === p2.x && p1.y === p2.y && p1.z === p2.z;
+}
+
 export function solvePart1(input: string): number {
   const hails = parse(input);
   const area =
@@ -84,6 +111,42 @@ export function solvePart1(input: string): number {
   return getCollidingWithinArea(hails, area);
 }
 export function solvePart2(input: string): number {
-  const data = parse(input);
-  return data.length;
+  const hails = parse(input);
+  let round = 0;
+  const positions: Point[][] = [];
+
+  while (true) {
+    positions.push(hails.map((h) => positionAt(h, round)));
+
+    for (let i1 = 0; i1 < hails.length; i1++) {
+      for (let i2 = 0; i2 < hails.length; i2++) {
+        if (i1 === i2) continue;
+
+        for (let t1 = 0; t1 < positions.length; t1++) {
+          for (let t2 = 0; t2 < positions.length; t2++) {
+            const p1 = positions[t1][i1];
+            const p2 = positions[t2][i2];
+
+            const f = getLineFormula(p1, p2, t1, t2);
+
+            const allGood = hails.every((hail, i) => {
+              if (i === i1 || i === i2) return true;
+              const res = positions
+                .map((position) => position[i])
+                .some((p, t) => equals(f(t), p));
+
+              return res;
+            });
+
+            if (allGood) {
+              const res = f(0);
+              return res.x + res.y + res.z;
+            }
+          }
+        }
+      }
+    }
+
+    round += 1;
+  }
 }
